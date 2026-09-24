15932e12f6a0fe0555193bc9d29cc5a1581c3b43
AdmerPRO <144557744+AdmerPRO@users.noreply.github.com>
2026-09-24 17:07:30 +0200

    fix: select CLI binary when packaging Linux Nightly

--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -19,7 +19,7 @@ command -v zip >/dev/null
 
 if [[ "${1:-}" != "--skip-build" ]]; then
     cargo rustc --manifest-path "$compiler_root/Cargo.toml" --locked --release \
-        -p adamantium-cli -- -C target-feature=+crt-static
+        -p adamantium-cli --bin adamantium -- -C target-feature=+crt-static
 fi
 
 rm -rf "$package_root"
